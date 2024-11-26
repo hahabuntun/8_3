dd81418f8983a3d8a354f771eebeb0198b9672b3
hahabuntun <hahabuntun@gmail.com>
2024-11-26 05:08:20 +0300

    added data class and datasource

--- app/src/main/java/com/example/fitnes/model/DailyTip.kt
+++ app/src/main/java/com/example/fitnes/model/DailyTip.kt
@@ -1,2 +1,7 @@
 package com.example.fitnes.model
 
+data class DailyTip(
+    val day : Int,
+    val image: Int,
+    val tip: Int
+)
--- app/src/main/java/com/example/fitnes/model/DailyTipsDataSource.kt
+++ app/src/main/java/com/example/fitnes/model/DailyTipsDataSource.kt
@@ -1,2 +1,134 @@
 package com.example.fitnes.model
 
+import com.example.fitnes.R
+
+object DailyTipsDataSource{
+    val dailyTips = listOf(
+        DailyTip(
+            day = R.string.day1,
+            image = R.drawable.pexels_victor_freitas_2261477,
+            tip = R.string.tip1
+        )
+        ,DailyTip(
+            day = R.string.day2,
+            image = R.drawable.pexels_victor_freitas_2261481,
+            tip = R.string.tip2
+        ),DailyTip(
+            day = R.string.day3,
+            image = R.drawable.pexels_victor_freitas_2261482,
+            tip = R.string.tip3
+        ),DailyTip(
+            day = R.string.day4,
+            image = R.drawable.pexels_victor_freitas_2261483,
+            tip = R.string.tip4
+        ),DailyTip(
+            day = R.string.day5,
+            image = R.drawable.pexels_victor_freitas_2261485,
+            tip = R.string.tip5
+        ),DailyTip(
+            day = R.string.day6,
+            image = R.drawable.pexels_victor_freitas_2261486,
+            tip = R.string.tip6
+        ),DailyTip(
+            day = R.string.day7,
+            image = R.drawable.pexels_victor_freitas_791765,
+            tip = R.string.tip7
+        ),DailyTip(
+            day = R.string.day8,
+            image = R.drawable.pexels_victor_freitas_897061,
+            tip = R.string.tip8
+        ),DailyTip(
+            day = R.string.day9,
+            image = R.drawable.pexels_victor_freitas_931326,
+            tip = R.string.tip9
+        ),DailyTip(
+            day = R.string.day10,
+            image = R.drawable.pexels_victor_freitas_949130,
+            tip = R.string.tip10
+        ),
+        DailyTip(
+            day = R.string.day11,
+            image = R.drawable.pexels_victor_freitas_2261477,
+            tip = R.string.tip11
+        )
+        ,DailyTip(
+            day = R.string.day12,
+            image = R.drawable.pexels_victor_freitas_2261481,
+            tip = R.string.tip12
+        ),DailyTip(
+            day = R.string.day13,
+            image = R.drawable.pexels_victor_freitas_2261482,
+            tip = R.string.tip13
+        ),DailyTip(
+            day = R.string.day14,
+            image = R.drawable.pexels_victor_freitas_2261483,
+            tip = R.string.tip14
+        ),DailyTip(
+            day = R.string.day15,
+            image = R.drawable.pexels_victor_freitas_2261485,
+            tip = R.string.tip15
+        ),DailyTip(
+            day = R.string.day16,
+            image = R.drawable.pexels_victor_freitas_2261486,
+            tip = R.string.tip16
+        ),DailyTip(
+            day = R.string.day17,
+            image = R.drawable.pexels_victor_freitas_791765,
+            tip = R.string.tip17
+        ),DailyTip(
+            day = R.string.day18,
+            image = R.drawable.pexels_victor_freitas_897061,
+            tip = R.string.tip18
+        ),DailyTip(
+            day = R.string.day19,
+            image = R.drawable.pexels_victor_freitas_931326,
+            tip = R.string.tip19
+        ),DailyTip(
+            day = R.string.day20,
+            image = R.drawable.pexels_victor_freitas_949130,
+            tip = R.string.tip20
+        ),
+        DailyTip(
+            day = R.string.day21,
+            image = R.drawable.pexels_victor_freitas_2261477,
+            tip = R.string.tip21
+        )
+        ,DailyTip(
+            day = R.string.day22,
+            image = R.drawable.pexels_victor_freitas_2261481,
+            tip = R.string.tip22
+        ),DailyTip(
+            day = R.string.day23,
+            image = R.drawable.pexels_victor_freitas_2261482,
+            tip = R.string.tip23
+        ),DailyTip(
+            day = R.string.day24,
+            image = R.drawable.pexels_victor_freitas_2261483,
+            tip = R.string.tip24
+        ),DailyTip(
+            day = R.string.day25,
+            image = R.drawable.pexels_victor_freitas_2261485,
+            tip = R.string.tip25
+        ),DailyTip(
+            day = R.string.day26,
+            image = R.drawable.pexels_victor_freitas_2261486,
+            tip = R.string.tip26
+        ),DailyTip(
+            day = R.string.day27,
+            image = R.drawable.pexels_victor_freitas_791765,
+            tip = R.string.tip27
+        ),DailyTip(
+            day = R.string.day28,
+            image = R.drawable.pexels_victor_freitas_897061,
+            tip = R.string.tip28
+        ),DailyTip(
+            day = R.string.day29,
+            image = R.drawable.pexels_victor_freitas_931326,
+            tip = R.string.tip29
+        ),DailyTip(
+            day = R.string.day30,
+            image = R.drawable.pexels_victor_freitas_949130,
+            tip = R.string.tip30
+        )
+    )
+}
